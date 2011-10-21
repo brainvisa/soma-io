7f3fefa42d0dfc6c42daafa681500fa2933f09a2
sapetnioc <yann@cointepas.net>
2011-10-21 15:05:42 +0000

    wip
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-io/trunk@45282 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ src/libsoma-io/soma/wip/type_descriptor.cpp
@@ -0,0 +1,9 @@
+#include <soma/type_descriptor.hpp>
+
+namespace soma {
+
+TypeDescriptor::~TypeDescriptor()
+{
+}
+
+} // namespace soma
---
+++ src/libsoma-io/soma/wip/type_descriptor.hpp
@@ -0,0 +1,16 @@
+#ifndef SOMA_TYPE_DESCRIPTOR
+#define SOMA_TYPE_DESCRIPTOR
+
+
+namespace soma {
+
+class TypeDescriptor
+{
+public:
+  virtual ~TypeDescriptor();
+};
+
+
+} // namespace soma
+
+#endif // SOMA_TYPE_DESCRIPTOR
