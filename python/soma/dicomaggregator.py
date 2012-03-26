2584c421fd18fa636a23499a4afad5dda58cf12d
martini <martini@bioproj.invalid>
2012-03-26 10:25:51 +0000

    #4407: changed source management
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/soma/soma-io/trunk@48270 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/soma/dicomaggregator.py
+++ python/soma/dicomaggregator.py
@@ -4,34 +4,32 @@ import os, dicom
 
 class DicomAggregator( object ):
     def __init__( self ):
-        self._dicom_sources = set()
+        self._dicom_sources = []
 
     def add_dicom_sources( self, sources ):
         for source in sources:
-            self._dicom_sources.add( source )
+            if os.path.isdir( source ):
+                for root, dirs, files in os.walk( source ):
+                    for name in files:
+                        self._dicom_sources.append( os.path.join( root, name ) )
+            else:
+                self._dicom_sources.append( source )
+        self._dicom_sources = list( set( self._dicom_sources ) )
 
     def aggregate( self ):
         self._aggregate_sources = {}
         serie_sop_uids = {}
-        for source in self._dicom_sources:
-            filelist = []
-            if os.path.isdir( source ):
-                for root, dirs, files in os.walk( source ):
-                    for name in files:
-                        filelist.append( os.path.join( root, name ) )
-            else:
-                filelist = [ source ]
-            for f in filelist:
-                try:
-                    ds = dicom.read_file( f )
-                except:
-                    continue
-                serie_uid = ds.SeriesInstanceUID
-                if not self._aggregate_sources.has_key( serie_uid ):
-                    self._aggregate_sources[ serie_uid ] = []
-                    serie_sop_uids[ serie_uid ] = []
+        for f in self._dicom_sources:
+            try:
+                ds = dicom.read_file( f )
+            except:
+                continue
+            serie_uid = ds.SeriesInstanceUID
+            if not self._aggregate_sources.has_key( serie_uid ):
+                self._aggregate_sources[ serie_uid ] = []
+                serie_sop_uids[ serie_uid ] = []
 
-                sop_uid = ds.SOPInstanceUID
-                if not sop_uid in serie_sop_uids[ serie_uid ]:
-                    serie_sop_uids[ serie_uid ].append( sop_uid )
-                    self._aggregate_sources[ serie_uid ].append( f )
+            sop_uid = ds.SOPInstanceUID
+            if not sop_uid in serie_sop_uids[ serie_uid ]:
+                serie_sop_uids[ serie_uid ].append( sop_uid )
+                self._aggregate_sources[ serie_uid ].append( f )
